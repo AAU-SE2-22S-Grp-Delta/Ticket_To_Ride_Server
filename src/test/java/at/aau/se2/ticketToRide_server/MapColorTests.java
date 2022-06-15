2a9f19826f044ed03025837eb291ff51eca1070a
SampaiWasTaken <samuel.radler@htl-klu.at>
2022-06-15 10:59:26 +0200

    MapColor tests

---
+++ src/test/java/at/aau/se2/ticketToRide_server/MapColorTests.java
@@ -0,0 +1,28 @@
+package at.aau.se2.ticketToRide_server;
+import at.aau.se2.ticketToRide_server.dataStructures.Destination;
+import at.aau.se2.ticketToRide_server.dataStructures.MapColor;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+public class MapColorTests
+{
+
+    @Test
+    void testGetByString()
+    {
+        assertEquals(MapColor.BLUE, MapColor.getByString("blue"));
+        assertEquals(MapColor.BLACK, MapColor.getByString("black"));
+        assertEquals(MapColor.GRAY, MapColor.getByString("gray"));
+        assertEquals(MapColor.GREEN, MapColor.getByString("green"));
+        assertEquals(MapColor.ORANGE, MapColor.getByString("orange"));
+        assertEquals(MapColor.PINK, MapColor.getByString("pink"));
+        assertEquals(MapColor.RED, MapColor.getByString("red"));
+        assertEquals(MapColor.WHITE, MapColor.getByString("white"));
+        assertEquals(MapColor.YELLOW, MapColor.getByString("yellow"));
+
+
+    }
+
+}
