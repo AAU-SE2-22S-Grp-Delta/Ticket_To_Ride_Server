64c032d0ce7f370873e54091d9ead21e7821226a
Christian Schönberg <1726822+chrisch1974@users.noreply.github.com>
2022-06-16 20:04:58 +0200

    SonarCloud: Optimize Logger

--- src/main/java/at/aau/se2/tickettoride_server/Logger.java
+++ src/main/java/at/aau/se2/tickettoride_server/Logger.java
@@ -3,18 +3,21 @@ package at.aau.se2.tickettoride_server;
 import at.aau.se2.tickettoride_server.server.Configuration_Constants;
 
 public class Logger {
+    private Logger() {
+    }
+
     public static void debug(String msg) {
         if (Configuration_Constants.DEBUG) {
-            System.out.println("(DEBUG)\t" + msg);
+            log("(DEBUG)\t" + msg);
         }
     }
 
     public static void exception(String msg) {
-        System.out.println("(EXCEPTION)\t" + msg);
+        log("(EXCEPTION)\t" + msg);
     }
 
     public static void fatal(String msg) {
-        System.out.println("(FATAL)\t" + msg);
+        log("(FATAL)\t" + msg);
     }
 
     public static void log(String msg) {
@@ -23,7 +26,7 @@ public class Logger {
 
     public static void verbose(String msg) {
         if (Configuration_Constants.VERBOSE) {
-            System.out.println("(VERBOSE)\t" + msg);
+            log("(VERBOSE)\t" + msg);
         }
     }
 }
