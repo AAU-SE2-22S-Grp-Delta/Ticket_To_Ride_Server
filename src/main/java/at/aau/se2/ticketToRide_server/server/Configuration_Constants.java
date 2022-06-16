2258dea3cb95d8c0b6d3d583233815258eba25b3
Christian Schönberg <1726822+chrisch1974@users.noreply.github.com>
2022-06-16 11:40:14 +0200

    SonarCloud: Fixed "Make verbose a static final constant or non-public and provide accessors if needed."

--- src/main/java/at/aau/se2/ticketToRide_server/server/Configuration_Constants.java
+++ src/main/java/at/aau/se2/ticketToRide_server/server/Configuration_Constants.java
@@ -1,8 +1,8 @@
 package at.aau.se2.ticketToRide_server.server;
 
 public class Configuration_Constants {
-    public static boolean verbose = true;
-    public static boolean debug = true;
-    public static boolean echo = true;
-    public static boolean doubleRails = false;
+    public static final boolean verbose = true;
+    public static final boolean debug = true;
+    public static final boolean echo = true;
+    public static final boolean doubleRails = false;
 }
