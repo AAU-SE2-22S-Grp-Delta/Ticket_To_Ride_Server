a1bfdb62326ca768b9aa0802732e26eeb25092b1
Christian Schönberg <1726822+chrisch1974@users.noreply.github.com>
2022-06-16 21:10:07 +0200

    Added missing MapColor tests

--- src/test/java/at/aau/se2/tickettoride_server/MapColorTests.java
+++ src/test/java/at/aau/se2/tickettoride_server/MapColorTests.java
@@ -4,6 +4,8 @@ import at.aau.se2.tickettoride_server.datastructures.MapColor;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
 class MapColorTests
 {
     @Test
@@ -18,7 +20,11 @@ class MapColorTests
         assertEquals(MapColor.RED, MapColor.getByString("red"));
         assertEquals(MapColor.WHITE, MapColor.getByString("white"));
         assertEquals(MapColor.YELLOW, MapColor.getByString("yellow"));
+        assertNull(MapColor.getByString("no_color"));
+    }
 
-
+    @Test
+    void testToString() {
+        assertEquals("black", MapColor.BLACK.toString());
     }
 }
